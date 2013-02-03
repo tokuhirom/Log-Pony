f7d88d5f4ba77e88788453f2a952ed04a9b88afe
tokuhirom <tokuhirom@gmail.com>
2013-02-03 11:21:30 +0900

    initial import

---
+++ lib/Log/Pony.pm
@@ -0,0 +1,41 @@
+package Log::Pony;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Log::Pony - ...
+
+=head1 SYNOPSIS
+
+  use Log::Pony;
+
+=head1 DESCRIPTION
+
+Log::Pony is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
